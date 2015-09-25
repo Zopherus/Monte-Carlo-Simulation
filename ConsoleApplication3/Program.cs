9f469c135df25949049f3a4f98df7ea2a26d2289
Zopherus <ejz1999@yahoo.com>
2015-09-24 20:21:07 -0700

    commit

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -8,45 +8,36 @@ namespace ConsoleApplication3
 {
     class Program
     {
-        const int NUMSTUDENTS = 10;
+        const int NUMSTUDENTS = 100;
         const int NUMTRIALS = 10000000;
-        static Student[] students = new Student[NUMSTUDENTS];
+        static int[] students = new int[NUMSTUDENTS];
         static Random random = new Random();
-        static int trialTrue = 0;
-        static int trialFalse = 0;
         static void Main(string[] args)
         {
-            for (int i = 0; i < NUMTRIALS; i++)
+            long successes = 0;
+            long i;
+            for (i = 0; i < NUMTRIALS; i++)
             {
-                trial();
-                Console.WriteLine((double)trialTrue / ((double)trialTrue + (double)trialFalse));
+                if (trial())
+                    successes++;
             }
-            Console.WriteLine(trialTrue);
-            Console.WriteLine(trialFalse);
-            Console.WriteLine((double)trialTrue / ((double)trialTrue + (double)trialFalse));
+            Console.WriteLine((double)successes / (double)i);
             Console.ReadLine();
         }
 
-        static void trial()
+        static bool trial()
         {
-            for (int a = 0; a < NUMSTUDENTS; a++)
-            {
-                students[a] = new Student();
-            }
             List<int> values = Enumerable.Range(0, NUMSTUDENTS).ToList<int>();
             int max = values.Count;
             for (int counter = 0; counter < max; counter++)
             {
                 int number = values.ElementAt(random.Next(values.Count));
                 if (counter == max - 1 && number == max - 1)
-                {
-                    trialFalse++;
-                    return;
-                }
+                    return false;
                 if (number != counter)
                 {
                     values.Remove(number);
-                    students[counter].nextStudent = number;
+                    students[counter] = number;
                 }
                 else
                     counter--;
@@ -55,20 +46,18 @@ namespace ConsoleApplication3
             bool result = true;
             for (int x = 0; x < NUMSTUDENTS; x++)
             {
-                if (!students[x].visited)
+                if (students[x] != -1)
                     result = false;
             }
-            if (result)
-                trialTrue++;
-            else
-                trialFalse++;
+            return result;
         }
 
         static void floodFill(int studentNumber)
         {
-            students[studentNumber].visited = true;
-            if (!students[students[studentNumber].nextStudent].visited)
-                floodFill(students[studentNumber].nextStudent);
+            int nextStudent = students[studentNumber];
+            students[studentNumber] = -1;
+            if (students[nextStudent] != -1)
+                floodFill(nextStudent);
         }
     }
 }
